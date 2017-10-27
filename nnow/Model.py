3710380074535bb8433c14fd1d4fbafdfaa7dc9c
Benjamin Peters <bpeters@coli.uni-saarland.de>
2017-10-27 08:34:07 +0200

    initial commit

---
+++ nnow/Model.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+
+class Embeddings(nn.Module):
+    pass
+
+
+class Encoder(nn.Module):
+    pass
+
+
+class Decoder(nn.Module):
+    pass
+
+
+class Seq2Seq(nn.Module):
+    pass
