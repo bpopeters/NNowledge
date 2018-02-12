22c15be1f503bf60022da3b7f1695a216b9cddf0
Benjamin Peters <bpeters@coli.uni-saarland.de>
2018-02-12 13:05:48 +0100

    add validation

--- train.py
+++ train.py
@@ -5,8 +5,10 @@ import torch
 import torch.optim as optim
 from torch.autograd import Variable
 import torch.nn as nn
-from nnow.Model import Encoder, Decoder, Seq2Seq, LogSoftmaxOutput
+from nnow.Model import Encoder, Decoder, Seq2Seq
+from nnow.OutputLayer import LogSoftmaxOutput
 from nnow.RNN import RNN
+from nnow.Attention import Attention
 
 
 def train_epoch(model, optimizer, criterion, batches):
@@ -20,12 +22,26 @@ def train_epoch(model, optimizer, criterion, batches):
         loss = criterion(predicted, gold)
         loss.backward()
         optimizer.step()
+    # model.eval()
+
+
+def validate_model(model, criterion, batches):
     model.eval()
+    pred = []
+    gold = []
+    for src, tgt in batches:
+        s, t = Variable(src), Variable(tgt)
+
+        pred.append(model(s, t))
+        gold.append(t[:, 1:].contiguous().view(-1))
+    loss = criterion(torch.cat(pred), torch.cat(gold))
+    return torch.exp(loss)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('train', type=torch.load)
+    parser.add_argument('valid', type=torch.load)
     parser.add_argument('-batch_size', type=int, default=64)
     parser.add_argument('-brnn', action='store_true')
     parser.add_argument('-word_vec_size', type=int, default=150)
@@ -49,10 +65,12 @@ def main():
         batch_first=True, dropout=opt.dropout
     )
 
+    attn = Attention(opt.hidden_size)
+
     dec_rnn = RNN(
         opt.rnn_type, bidirectional=False, num_layers=opt.layers,
         input_size=opt.word_vec_size, hidden_size=opt.hidden_size,
-        batch_first=True, dropout=opt.dropout
+        batch_first=True, dropout=opt.dropout, attn=attn
     )
 
     output_layer = LogSoftmaxOutput(
@@ -63,12 +81,15 @@ def main():
     decoder = Decoder(tgt_emb, dec_rnn, output_layer)
     model = Seq2Seq(encoder, decoder)
 
-    criterion = nn.NLLLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+    criterion = nn.NLLLoss(ignore_index=0)
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
     for i in range(1, opt.epochs + 1):
-        batches = opt.train.batches(opt.batch_size)
-        train_epoch(model, optimizer, criterion, batches)
+        train_batches = opt.train.batches(opt.batch_size)
+        valid_batches = opt.valid.batches(opt.batch_size)
+        train_epoch(model, optimizer, criterion, train_batches)
+        print(validate_model(model, criterion, valid_batches))
+    torch.save(model, 'foo.pt')
 
 
 if __name__ == '__main__':
