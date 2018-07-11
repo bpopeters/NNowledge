4d3b08977c176620d8e69f042c1095125b2921ad
bpopeters <bpeters@coli.uni-saarland.de>
2018-07-12 00:29:51 +0100

    update for pandas data structures

--- preprocess.py
+++ preprocess.py
@@ -1,30 +1,40 @@
 #!/usr/bin/env python
 
 """
-preprocess.py produces the Vocab objects necessary for converting data
-between text and tensors. It accepts arbitrarily many of each source and
-target files.
+preprocess.py produces the stuff necessary for converting data
+between text and tensors and iterating through a dataset in batches
 """
 
 import argparse
 from itertools import chain
 import torch
-from nnow.IO import Vocab, tokenize, BitextIterator
+from nnow.IO import tokenize, Vocab, BitextDataset
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-src', nargs='+')
-    parser.add_argument('-tgt', nargs='+')
+    parser.add_argument('train_src')
+    parser.add_argument('valid_src')
+    parser.add_argument('train_tgt')
+    parser.add_argument('valid_tgt')
     parser.add_argument('-out', default="")
     opt = parser.parse_args()
-    src = chain.from_iterable([tokenize(s) for s in opt.src])
-    tgt = chain.from_iterable([tokenize(t) for t in opt.tgt])
+    src_train_tokens = tokenize(opt.train_src)
+    src_valid_tokens = tokenize(opt.valid_src)
+    tgt_train_tokens = tokenize(opt.train_tgt)
+    tgt_valid_tokens = tokenize(opt.valid_tgt)
 
-    src_vocab = Vocab(src)
-    tgt_vocab = Vocab(tgt)
-    bt = BitextIterator(src_vocab, tgt_vocab)
-    torch.save(bt, opt.out + '_vocab.pt')
+    src_vocab = Vocab(chain(src_train_tokens, src_valid_tokens))
+    tgt_vocab = Vocab(chain(tgt_train_tokens, tgt_valid_tokens))
+    train_dataset = BitextDataset(
+        src_train_tokens, tgt_train_tokens, src_vocab, tgt_vocab
+    )
+    valid_dataset = BitextDataset(
+        src_valid_tokens, tgt_valid_tokens, src_vocab, tgt_vocab
+    )
+    torch.save(
+        {'train': train_dataset, 'valid': valid_dataset}, opt.out + '_data.pt'
+    )
 
 if __name__ == '__main__':
     main()
