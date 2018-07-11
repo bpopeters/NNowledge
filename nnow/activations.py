729be8849d0fa5c51d561c3bf20edc01e20ab0d7
bpopeters <bpeters@coli.uni-saarland.de>
2018-07-11 16:44:41 +0100

    add support for sparsemax on arbitrary-dimensional tensors

--- nnow/activations.py
+++ nnow/activations.py
@@ -3,46 +3,57 @@ from torch.autograd import Function
 import torch.nn as nn
 
 
-def threshold_and_support(z):
-    sorted_z, _ = torch.sort(z, descending=True, dim=1)
-    z_sum = sorted_z.cumsum(1) - 1  # sort of a misnomer
-    support = torch.arange(1, sorted_z.size(1) + 1) * sorted_z > z_sum
-    k_z = support.sum(dim=1)
-    tau_z = z_sum.gather(1, k_z.unsqueeze(1) - 1).squeeze() / k_z.float()
-    return tau_z.unsqueeze(1), k_z
+def threshold_and_support(z, dim=0):
+    """
+    z: any dimension
+    dim: dimension along which to apply the sparsemax
+    """
+    sorted_z, _ = torch.sort(z, descending=True, dim=dim)
+    z_sum = sorted_z.cumsum(dim) - 1  # sort of a misnomer
+    K = torch.arange(1, sorted_z.size(dim) + 1).view(
+        torch.Size([-1] + [1] * (z.dim() - 1))
+    ).transpose(0, dim)
+    support = K * sorted_z > z_sum
+
+    k_z_indices = support.sum(dim=dim).unsqueeze(dim)
+    k_z = k_z_indices.float()
+    tau_z = z_sum.gather(dim, k_z_indices - 1) / k_z
+    return tau_z, k_z
 
 
 class SparsemaxFunction(Function):
 
     @staticmethod
-    def forward(ctx, z):
+    def forward(ctx, input, dim=0):
         """
-        z (FloatTensor) batch x hidden size
-        (long term: it would be great if it handled any shape)
+        input (FloatTensor): any shape
+        returns (FloatTensor): same shape with sparsemax computed on given dim
         """
-        tau_z, k_z = threshold_and_support(z)
-        output = torch.clamp(z - tau_z, min=0)
+        ctx.dim = dim
+        tau_z, k_z = threshold_and_support(input, dim=dim)
+        output = torch.clamp(input - tau_z, min=0)
         ctx.save_for_backward(k_z, output)
         return output
 
     @staticmethod
     def backward(ctx, grad_output):
         k_z, output = ctx.saved_tensors
+        dim = ctx.dim
         grad_input = grad_output.clone()
         grad_input[output == 0] = 0
 
-        v_hat = (grad_input.sum(dim=1) / k_z).unsqueeze(1)
+        v_hat = (grad_input.sum(dim=dim) / k_z.squeeze()).unsqueeze(dim)
         grad_input = torch.where(output != 0, grad_input - v_hat, grad_input)
-        return grad_input
+        return grad_input, None
 
 sparsemax = SparsemaxFunction.apply
 
 
 class Sparsemax(nn.Module):
 
-    def __init__(self):
+    def __init__(self, dim=0):
+        self.dim = dim
         super(Sparsemax, self).__init__()
 
     def forward(self, input):
-        # TODO: sparsemax along other dimensions
-        return sparsemax(input)
+        return sparsemax(input, dim=self.dim)
